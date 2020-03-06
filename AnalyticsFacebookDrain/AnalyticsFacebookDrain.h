5eaaba781d66c3bf61af0604b58e50d743b8fc7d
sroik <vasili.kazhanouski@gmail.com>
2020-03-06 21:14:42 +0300

    fixes (#4)

--- AnalyticsFacebookDrain/AnalyticsFacebookDrain.h
+++
@@ -1,8 +0,0 @@
-//
-//  Copyright © 2020 sroik. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double AnalyticsFacebookDrainVersionNumber;
-FOUNDATION_EXPORT const unsigned char AnalyticsFacebookDrainVersionString[];
