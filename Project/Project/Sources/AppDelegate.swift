8edad97d243a01a99cf620142048118ef516cfca
sroik <vasili.kazhanouski@gmail.com>
2020-03-06 18:44:46 +0300

    tweaks

---
+++ Project/Project/Sources/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  Copyright © 2020 sroik. All rights reserved.
+//
+
+import UIKit
+import UtilsCore
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        guard Environment.isTests == false else {
+            return false
+        }
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Project/ProjectTests/ProjectTests.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2020 sroik. All rights reserved.
+//
+
+import XCTest
+@testable import Project
+
+class ProjectTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
