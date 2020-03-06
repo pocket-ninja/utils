877bd63c6437a07eec8c7581090b1c23476f6de4
sroik <vasili.kazhanouski@gmail.com>
2020-03-06 21:33:27 +0300

    fixes

---
+++ Utils/Analytics/Core/SupportingFiles/Analytics.h
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2020 sroik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double AnalyticsVersionNumber;
+FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
+
