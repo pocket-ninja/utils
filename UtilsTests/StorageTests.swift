0d03e6718c058b6288967e3dbcd9167472cf39ca
sroik <vasili.kazhanouski@gmail.com>
2020-03-07 20:26:14 +0300

    storage tests

---
+++ UtilsTests/StorageTests.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2019 appcraft. All rights reserved.
+//
+
+import XCTest
+import UtilsCore
+
+class StorageTests: XCTestCase {
+    func testSavesUUID() {
+        let uuid = UUID()
+        let storage = StorageMock()
+
+        storage.set(uuid, forKey: "test_key")
+
+        XCTAssertEqual(storage.values["test_key"] as? String, uuid.uuidString)
+    }
+
+    func testLoadsUUID() {
+        let uuid = UUID()
+        let storage = StorageMock()
+
+        storage.set(uuid, forKey: "test_key")
+
+        XCTAssertEqual(storage.uuid(forKey: "test_key"), uuid)
+    }
+}
