3f59bf17c441b774a5ebcbb0354be1512bae2488
sroik <vasili.kazhanouski@gmail.com>
2020-03-10 18:31:06 +0300

    remove reactive comps

--- Utils/UtilsCore/Sources/Extensions/UIKit/UIView+Extensions.swift
+++ Utils/UtilsCore/Sources/Extensions/UIKit/UIView+Extensions.swift
@@ -8,19 +8,4 @@ public extension UIView {
     func addSubviews(_ subviews: UIView...) {
         subviews.forEach(addSubview(_:))
     }
-
-    func subscribeToTaps(ofCount tapsCount: Int = 1, with action: @escaping () -> Void) -> Token {
-        let action = GestureTarget(states: [.ended]) { _ in
-            action()
-        }
-
-        let gesture = UITapGestureRecognizer(target: action, action: #selector(action.gestureAction(_:)))
-        gesture.numberOfTapsRequired = tapsCount
-        addGestureRecognizer(gesture)
-
-        return DeinitToken { [weak self] in
-            self?.removeGestureRecognizer(gesture)
-            _ = action
-        }
-    }
 }
