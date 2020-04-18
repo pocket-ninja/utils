73a5d4919118fb950c3e7ca29c96d2771a472e04
sroik <vasili.kazhanouski@gmail.com>
2020-04-18 13:42:54 +0300

    matrix

---
+++ Utils/UtilsCore/Sources/DataStructs/Matrix.swift
@@ -0,0 +1,159 @@
+//
+//  Copyright © 2020 sroik. All rights reserved.
+//
+
+import Foundation
+
+public struct Matrix<T> {
+    public let rows: Int
+    public let columns: Int
+    public private(set) var grid: [T]
+
+    public var count: Int {
+        return rows * columns
+    }
+
+    public init() {
+        rows = 0
+        columns = 0
+        grid = [T]()
+    }
+
+    public init(rows: Int, columns: Int, repeating value: T) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: value, count: rows * columns)
+    }
+
+    public init?(rows: Int, columns: Int, grid: [T]) {
+        guard grid.count == columns * rows else {
+            return nil
+        }
+
+        self.rows = rows
+        self.columns = columns
+        self.grid = grid
+    }
+
+    public init?(array: [[T]]) {
+        rows = array.count
+        columns = array.first?.count ?? 0
+        grid = [T]()
+
+        for row in array {
+            guard row.count == columns else {
+                return nil
+            }
+
+            grid.append(contentsOf: row)
+        }
+    }
+
+    public subscript(row: Int, column: Int) -> T {
+        get {
+            return grid[(row * columns) + column]
+        }
+        set {
+            grid[(row * columns) + column] = newValue
+        }
+    }
+
+    public subscript(row: Int) -> [T] {
+        let start = row * columns
+        let end = start + columns
+        return Array(grid[start ..< end])
+    }
+}
+
+public extension Matrix {
+    typealias EnumerationBlock = (_ row: Int, _ column: Int, _ element: T) -> Void
+    typealias Position = MatrixPosition
+
+    subscript(position: Position) -> T {
+        get {
+            return self[position.row, position.column]
+        }
+        set {
+            self[position.row, position.column] = newValue
+        }
+    }
+
+    subscript(safe position: Position) -> T? {
+        return self[safe: position.row, position.column]
+    }
+
+    subscript(safe row: Int, column: Int) -> T? {
+        return grid[safe: (row * columns) + column]
+    }
+
+    subscript(safe row: Int) -> [T]? {
+        return (0 ..< rows).contains(row) ? self[row] : nil
+    }
+
+    subscript(row row: Int) -> [T] {
+        return self[row]
+    }
+
+    subscript(safeRow row: Int) -> [T]? {
+        return self[safe: row]
+    }
+
+    subscript(column column: Int) -> [T] {
+        let end = count - (columns - column)
+        return stride(from: column, through: end, by: columns).map { grid[$0] }
+    }
+
+    subscript(safeColumn column: Int) -> [T]? {
+        return (0 ..< columns).contains(column) ? self[column: column] : nil
+    }
+
+    func array() -> [[T]] {
+        return grid.chunks(columns)
+    }
+
+    func isValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+
+    func isValid(position: Position) -> Bool {
+        return isValid(row: position.row, column: position.column)
+    }
+
+    func enumerate(_ block: EnumerationBlock) {
+        for r in 0 ..< rows {
+            for c in 0 ..< columns {
+                block(r, c, self[r, c])
+            }
+        }
+    }
+
+    func map<E>(_ transform: (T) throws -> E) rethrows -> Matrix<E> {
+        return try Matrix<E>(rows: rows, columns: columns, grid: grid.map(transform))!
+    }
+
+    func forEach(_ body: (T) throws -> Void) rethrows {
+        try grid.forEach(body)
+    }
+
+    mutating func transform(using block: (inout T) -> Void) {
+        grid.transform(using: block)
+    }
+
+    func first(where predicate: (T) throws -> Bool) rethrows -> T? {
+        return try grid.first(where: predicate)
+    }
+}
+
+public struct MatrixPosition: Codable, Hashable {
+    public var row: Int
+    public var column: Int
+
+    public init(row: Int, column: Int) {
+        self.row = row
+        self.column = column
+    }
+}
+
+extension Matrix: Equatable where T: Equatable {}
+extension Matrix: Hashable where T: Hashable {}
+extension Matrix: Codable where T: Codable {}
---
+++ UtilsTests/Sources/UtilsCore/MatrixTests.swift
@@ -0,0 +1,131 @@
+//
+//  Copyright © 2020 sroik. All rights reserved.
+//
+
+import XCTest
+import UtilsCore
+
+class MatrixTests: XCTestCase {
+    func testInitialization() {
+        let m = Matrix<Int>(rows: 2, columns: 3, repeating: 5)
+        XCTAssertEqual(m.grid, [5, 5, 5, 5, 5, 5])
+    }
+
+    func testIsValidRowAndColumn() {
+        let m = Matrix<Double>(rows: 3, columns: 3, repeating: 0)
+        for i in 0 ..< 3 {
+            for j in 0 ..< 3 {
+                XCTAssertTrue(m.isValid(row: i, column: j))
+            }
+        }
+
+        XCTAssertFalse(m.isValid(row: 0, column: 3))
+        XCTAssertFalse(m.isValid(row: 3, column: 0))
+    }
+
+    func testIsValidPosition() {
+        let m = Matrix<Double>(rows: 3, columns: 3, repeating: 0)
+        XCTAssertTrue(m.isValid(position: MatrixPosition(row: 0, column: 0)))
+        XCTAssertFalse(m.isValid(position: MatrixPosition(row: 0, column: 3)))
+        XCTAssertFalse(m.isValid(position: MatrixPosition(row: 3, column: 0)))
+    }
+
+    func testWrongFormatInitialization() {
+        XCTAssertNil(Matrix<Double>(array: [[1, 2, 3], [4, 5, 6], [7, 8]]))
+        XCTAssertNil(Matrix<Double>(rows: 2, columns: 1, grid: [1, 2, 3]))
+    }
+
+    func testMatrixComparing() {
+        let m = Matrix<Int>(rows: 2, columns: 2, repeating: 5)
+        let m2 = Matrix<Int>(rows: 4, columns: 1, repeating: 5)
+        XCTAssertTrue(m != m2)
+        XCTAssertTrue(m == Matrix(array: [[5, 5], [5, 5]])!)
+    }
+
+    func testToArray() throws {
+        let c = [[1, 2], [3, 4]]
+        let m = try Matrix(array: c).get()
+        XCTAssertTrue(m.array() == c)
+    }
+
+    func testSubscript() {
+        var m = Matrix(array: [[1, 2], [3, 4]])!
+        XCTAssertEqual(m[0, 0], 1)
+        XCTAssertEqual(m[0][0], 1)
+        XCTAssertEqual(m[0, 1], 2)
+        XCTAssertEqual(m[0][1], 2)
+        XCTAssertNil(m[safe: 2, 2])
+
+        m[0, 0] = 10
+        XCTAssertEqual(m[0, 0], 10)
+
+        let position = MatrixPosition(row: 0, column: 0)
+        m[position] = 11
+        XCTAssertEqual(m[position], 11)
+        XCTAssertNil(m[safe: Matrix.Position(row: -1, column: -1)])
+    }
+
+    func testEnumeration() {
+        let m = Matrix<Int>(array: [[0, 1, 2, 3, 4]])!
+        m.enumerate { row, column, element in
+            XCTAssertEqual(row, 0)
+            XCTAssertEqual(column, element)
+        }
+    }
+
+    func testMap() {
+        let m = Matrix<Int>(array: [[1, 2], [3, 4]])!
+        let mapped = m.map { $0 * 10 }
+        XCTAssert(mapped == Matrix<Int>(array: [[10, 20], [30, 40]])!)
+    }
+
+    func testTransform() {
+        var m = Matrix<Int>(array: [[1, 2], [3, 4]])!
+        m.transform { $0 *= 2 }
+        XCTAssertTrue(m == Matrix<Int>(array: [[2, 4], [6, 8]])!)
+    }
+
+    func testPredicate() {
+        let m = Matrix<Int>(array: [[1, 4], [9, 16]])!
+        let e: Int? = m.first { sqrt(Double($0)) == 3 }
+        XCTAssertEqual(e!, 9)
+    }
+
+    func testForEach() {
+        var sum = 0
+        let m = Matrix<Int>(array: [[1, 2], [3, 4]])!
+        m.forEach { e in
+            sum += e
+        }
+        XCTAssertEqual(sum, 1 + 2 + 3 + 4)
+    }
+
+    func testSubscriptRow() {
+        let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 1, 1]]
+        let m = Matrix<Int>(array: array)!
+        array.indices.forEach { idx in
+            XCTAssertEqual(array[idx], m[idx])
+            XCTAssertEqual(array[idx], m[row: idx])
+            XCTAssertNotNil(m[safe: idx])
+            XCTAssertNotNil(m[safeRow: idx])
+        }
+
+        XCTAssertNil(m[safe: 4])
+        XCTAssertNil(m[safe: -1])
+        XCTAssertNil(m[safeRow: 4])
+        XCTAssertNil(m[safeRow: -1])
+    }
+
+    func testSubscriptColumn() throws {
+        let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 1, 1]]
+        let m = try Matrix<Int>(array: array).get()
+
+        XCTAssertEqual(m[column: 1], [2, 5, 8, 1])
+        XCTAssertEqual(m[column: 2], [3, 6, 9, 1])
+        XCTAssertNotNil(m[column: 1])
+        XCTAssertNotNil(m[column: 2])
+
+        XCTAssertNil(m[safeColumn: 4])
+        XCTAssertNil(m[safeColumn: -1])
+    }
+}
