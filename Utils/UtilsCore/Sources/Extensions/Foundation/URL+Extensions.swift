851136aad25713340199d3fbfc325925da1f38f3
sroik <vasili.kazhanouski@gmail.com>
2026-01-22 14:34:29 +0300

    url extensions for appstore links

--- Utils/UtilsCore/Sources/Extensions/Foundation/URL+Extensions.swift
+++ Utils/UtilsCore/Sources/Extensions/Foundation/URL+Extensions.swift
@@ -1,23 +1,35 @@
 //
-//  Copyright © 2020 pocket-ninja. All rights reserved.
+//  Copyright © 2026 sroik. All rights reserved.
 //
 
 import Foundation
 
 public extension URL {
-    static func storeRate(appId: String) -> URL? {
-        return URL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=\(appId)")
+    @available(iOS 16.0, *)
+    static func appStoreReview(id: String) -> URL? {
+        appStore(id: id)?.appending(queryItems: [
+            .init(name: "action", value: "write-review")
+        ])
     }
-
-    static func store(appId: String, provider: String, campaign: String) -> URL? {
-        return URL(string: "https://apps.apple.com/app/apple-store/id\(appId)?pt=\(provider)&ct=\(campaign)&mt=8")
+    
+    @available(iOS 16.0, *)
+    static func appStore(id: String, provider: String, campaign: String) -> URL? {
+        appStore(id: id)?.appending(queryItems: [
+            .init(name: "pt", value: provider),
+            .init(name: "ct", value: campaign),
+            .init(name: "mt", value: "8")
+        ])
+    }
+    
+    static func appStore(id: String) -> URL? {
+        URL(string: "https://apps.apple.com/app/id\(id)")
     }
     
     static func instagramApp(id: String) -> URL? {
-        return URL(string: "instagram://user?username=\(id)")
+        URL(string: "instagram://user?username=\(id)")
     }
     
     static func instagramWeb(id: String) -> URL? {
-        return URL(string: "https://www.instagram.com/\(id)")
+        URL(string: "https://www.instagram.com/\(id)")
     }
 }
